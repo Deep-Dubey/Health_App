68c019568a67fe1bdde143216e875eee14005938
DEEP DUBEY <64470309+Deep-Dubey@users.noreply.github.com>
2023-08-24 13:51:15 +0530

    Updates

---
+++ precache-manifest.e86f6c89ef37e723422d8ba2a967e37c.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "a66c046e3321dd7b1e6f",
+    "url": "/Health_App/static/js/main.a66c046e.chunk.js"
+  },
+  {
+    "revision": "cd961b865d9d9dcdf2e4",
+    "url": "/Health_App/static/js/1.cd961b86.chunk.js"
+  },
+  {
+    "revision": "2f7318ff030345482109",
+    "url": "/Health_App/static/js/runtime~main.2f7318ff.js"
+  },
+  {
+    "revision": "0253daf3216862ce7df7bb30f860defc",
+    "url": "/Health_App/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/Health_App/precache-manifest.e86f6c89ef37e723422d8ba2a967e37c.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/Health_App/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
+});
